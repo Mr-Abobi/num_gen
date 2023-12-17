d0e92311f08d5d1069c34083770ea799d2e1db7c
Mr-Abobi <vokoff.yv@gmail.com>
2023-12-18 01:22:32 +0300

    1-th commit

---
+++ num_gen.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void main(){
+    setlocale(LC_ALL,"Rus");
+    cout << "начать? (y/n)";
+    char chouse1;
+    cin >> chouse1;
+    //if(chouse1 == "y")
+}
