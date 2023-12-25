0b8fb7c6df2eb4292bb2cae2988ef75833c83ff7
Mr-Abobi <vokoff.yv@gmail.com>
2023-12-25 17:35:13 +0300

    second commit

--- num_gen.cpp
+++ num_gen.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-void main(){
+int main(){
     setlocale(LC_ALL,"Rus");
-    cout << "начать? (y/n)";
-    char chouse1;
-    cin >> chouse1;
-    //if(chouse1 == "y")
+    // cout << "начать? (y/n)";
+    // char CHOUSE1;
+    // cin >> CHOUSE1;
+    // if(CHOUSE1 == "y")
+    char ch;
+    cout << "начать?\n";
+    cin >> ch;
+
+        if (ch == 'y')
+    {   
+        cout << "you press \"y\"\n";
+    }
+    else
+    {
+        exit;
+        //cout << "you press \"n\"\n";
+    }
+    return 0;
 }
